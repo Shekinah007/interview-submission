7746d4f5db95b5553e41b240a79cde290936b885
Shekinah Shalom-Hope <shalomsheks@gmail.com>
2023-12-30 21:41:12 +0100

    - Fix minor typos - Ensure responsiveness

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
 
   const [otp, setOtp] = useState("");
 
-
   const submitOtp = () => {
 
     if (digit1 && digit2 && digit3 && digit4 && digit5 && digit6) {
@@ -28,33 +27,34 @@ function App() {
     }
 
 
+
   }
 
   return (
     <div className="app">
-      <div className="top-bar"></div>
-      <div className='header'>
-        <h1>Verifty Your Account</h1>
-        <p className="gray">We are sending an OTP to verify your validate your
-          mobile number, Hang on!</p>
+      <div class="card">
+        {/* <div className="top-bar"></div> */}
+        <div className='header'>
+          <h1>Verify Your Account</h1>
+          <p className="gray">We are sending an OTP to validate your
+            mobile number, Hang on!</p>
+        </div>
+
+        <div className="otp">
+          <input type="text" value={digit1} maxLength={1} onChange={(e) => {
+            setDigit1(e.target.value)
+            console.log("Digit 1: ", digit1)
+          }} />
+          <input type="text" value={digit2} maxLength={1} onChange={(e) => setDigit2(e.target.value)} />
+          <input type="text" value={digit3} maxLength={1} onChange={(e) => setDigit3(e.target.value)} />
+          <input type="text" value={digit4} maxLength={1} onChange={(e) => setDigit4(e.target.value)} />
+          <input type="text" value={digit5} maxLength={1} onChange={(e) => setDigit5(e.target.value)} />
+          <input type="text" value={digit6} maxLength={1} onChange={(e) => setDigit6(e.target.value)} />
+        </div>
+
+        <p><span className="gray">A SMS has been sent to</span> 321-264-5231</p>
+        <button onClick={() => { submitOtp() }}>Submit</button>
       </div>
-
-      <div className="otp">
-        <input type="text" value={digit1} maxLength={1} onChange={(e) => {
-          setDigit1(e.target.value)
-          console.log("Digit 1: ", digit1)
-        }} />
-        <input type="text" value={digit2} maxLength={1} onChange={(e) => setDigit2(e.target.value)} />
-        <input type="text" value={digit3} maxLength={1} onChange={(e) => setDigit3(e.target.value)} />
-        <input type="text" value={digit4} maxLength={1} onChange={(e) => setDigit4(e.target.value)} />
-        <input type="text" value={digit5} maxLength={1} onChange={(e) => setDigit5(e.target.value)} />
-        <input type="text" value={digit6} maxLength={1} onChange={(e) => setDigit6(e.target.value)} />
-      </div>
-
-      <p><span className="gray">A SMS has been sent to</span> 303-345-2323</p>
-      <button onClick={() => { submitOtp() }}>Submit</button>
-
-
     </div>
   )
 }
