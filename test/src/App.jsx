24c454841e5d635c20e0352a653c7b0bf4107aa3
Shekinah Shalom-Hope <shalomsheks@gmail.com>
2023-12-30 22:12:31 +0100

    Check validate input type

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -25,9 +25,14 @@ function App() {
     } else {
       alert("Fill in all boxes")
     }
+  }
 
-
-
+  const checkNumber = (number, setter) => {
+    if (isNaN(number)) {
+      setter("")
+    } else {
+      setter(number)
+    }
   }
 
   return (
@@ -36,20 +41,30 @@ function App() {
         {/* <div className="top-bar"></div> */}
         <div className='header'>
           <h1>Verify Your Account</h1>
-          <p className="gray">We are sending an OTP to validate your
+          <p className="gray">We are sending a OTP to validate your
             mobile number, Hang on!</p>
         </div>
 
         <div className="otp">
           <input type="text" value={digit1} maxLength={1} onChange={(e) => {
-            setDigit1(e.target.value)
-            console.log("Digit 1: ", digit1)
+            checkNumber(e.target.value, setDigit1)
           }} />
-          <input type="text" value={digit2} maxLength={1} onChange={(e) => setDigit2(e.target.value)} />
-          <input type="text" value={digit3} maxLength={1} onChange={(e) => setDigit3(e.target.value)} />
-          <input type="text" value={digit4} maxLength={1} onChange={(e) => setDigit4(e.target.value)} />
-          <input type="text" value={digit5} maxLength={1} onChange={(e) => setDigit5(e.target.value)} />
-          <input type="text" value={digit6} maxLength={1} onChange={(e) => setDigit6(e.target.value)} />
+          <input type="text" value={digit2} maxLength={1} onChange={(e) => {
+            checkNumber(e.target.value, setDigit2)
+          }} />
+          <input type="text" value={digit3} maxLength={1} onChange={(e) => {
+            checkNumber(e.target.value, setDigit3)
+          }} />
+          <input type="text" value={digit4} maxLength={1} onChange={(e) => {
+            checkNumber(e.target.value, setDigit4)
+          }} />
+          <input type="text" value={digit5} maxLength={1} onChange={(e) => {
+            checkNumber(e.target.value, setDigit5)
+          }} />
+          <input type="text" value={digit6} maxLength={1} onChange={(e) => {
+            checkNumber(e.target.value, setDigit6)
+          }
+          } />
         </div>
 
         <p><span className="gray">A SMS has been sent to</span> 321-264-5231</p>
